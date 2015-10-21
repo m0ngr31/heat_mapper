ba776605613819c4e6037898af1d8058e540f0fc
Joe Ipson <joeipson@gmail.com>
2015-10-20 23:01:28 -0600

    Trying more stuff for heroku.

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,11 @@ Bundler.require(*Rails.groups)
 
 module HeatMapper
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.orm :mongoid
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
